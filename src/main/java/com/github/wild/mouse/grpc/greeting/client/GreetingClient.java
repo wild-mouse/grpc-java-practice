3097d33190e5cb27f886391ea5afc8b94f68c62a
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-01-01 21:22:54 +0900

    Add dummy client and server

---
+++ src/main/java/com/github/wild/mouse/grpc/greeting/client/GreetingClient.java
@@ -0,0 +1,23 @@
+package com.github.wild.mouse.grpc.greeting.client;
+
+import com.proto.dummy.DummyServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class GreetingClient {
+
+    public static void main(String[] args) {
+        System.out.println("Hello I'm a gRPC client");
+
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+
+        System.out.println("Creating stub");
+        DummyServiceGrpc.DummyServiceBlockingStub client =
+                DummyServiceGrpc.newBlockingStub(channel);
+
+        System.out.println("Shutting down channel");
+        channel.shutdown();
+    }
+}
